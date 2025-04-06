11ba5e4120e893a8f1a805bd4034102736538b30
Paul Hinterberger <Paul@4baiern.de>
2025-04-06 15:48:37 +0200

    validate input before submitting

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import {
   ThemeProvider,
   CssBaseline,
@@ -13,40 +12,72 @@ import {
   InputLabel,
   Select,
   MenuItem,
+  FormHelperText,
 } from "@mui/material";
 import theme from "./theme";
 import { MuiFileInput } from "mui-file-input";
 import moment from "moment-timezone";
 import { useMutation } from "@tanstack/react-query";
+import { bool, object, string } from "yup";
+import { useFormik } from "formik";
+
+const fileToBase64 = (file) => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = () => resolve(reader.result.split(",")[1]); // Remove data: prefix
+    reader.onerror = reject;
+    reader.readAsDataURL(file);
+  });
+};
+
+const installValidationSchema = object({
+  dbPassword: string().required("DB Password is required"),
+  email: string().email("Invalid email address").required("Email is required"),
+  timezone: string().required("Timezone is required"),
+  pteroUsername: string().required("Pterodactyl Username is required"),
+  firstName: string().required("First Name is required"),
+  lastName: string().required("Last Name is required"),
+  pteroPassword: string().required("Pterodactyl Password is required"),
+  cert: string(),
+  password: string().required("Password is required"),
+  serverIp: string().required("Server IP is required"),
+  agree: bool().oneOf([true], "You must agree to the terms and conditions"),
+});
 
 function App() {
-  const [agree, setAgree] = useState(false);
-
-  const [dbPassword, setDbPassword] = useState("");
-  const [email, setEmail] = useState("");
-  const [timezone, setTimezone] = useState("");
-  const [pteroUsername, setPteroUsername] = useState("");
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [pteroPassword, setPteroPassword] = useState("");
-  const [cert, setCert] = useState("");
-  const [password, setPassword] = useState("");
-  const [serverIp, setServerIp] = useState("");
-
   const timezones = moment.tz.names();
 
   const install = useMutation({
     mutationFn: (newTodo) => {
       return fetch(
         window.location.hostname === "localhost"
-          ? "http://localhost:8080"
-          : "https://pterodactyl-installer-server-619074647413.europe-west1.run.app",
+          ? "http://localhost:8080/install"
+          : "https://pterodactyl-installer-server-619074647413.europe-west1.run.app/install",
         {
           method: "POST",
           body: JSON.stringify(newTodo),
         },
       );
     },
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      dbPassword: "",
+      email: "",
+      timezone: "",
+      pteroUsername: "",
+      firstName: "",
+      lastName: "",
+      pteroPassword: "",
+      cert: "",
+      password: "",
+      agree: false,
+      serverIp: "",
+    },
+    validationSchema: installValidationSchema,
+    onSubmit: async (values) =>
+      install.mutate({ ...values, cert: await fileToBase64(values.cert) }),
   });
 
   return (
@@ -82,30 +113,47 @@ function App() {
                 </Typography>
 
                 <TextField
+                  name={"serverIp"}
                   label="Server IP"
                   fullWidth
                   margin="normal"
-                  value={serverIp}
-                  onChange={(e) => setServerIp(e.target.value)}
+                  value={formik.values.serverIp}
+                  onChange={formik.handleChange}
+                  error={
+                    formik.touched.serverIp && Boolean(formik.errors.serverIp)
+                  }
+                  helperText={formik.touched.serverIp && formik.errors.serverIp}
                 />
                 <MuiFileInput
                   InputProps={{ accept: ".pub, .crt" }}
                   margin="normal"
                   fullWidth
-                  value={cert}
+                  value={formik.values.cert}
                   onChange={(value) => {
-                    setPassword("");
-                    setCert(value);
+                    console.log(value);
+                    formik.handleChange({
+                      target: { name: "password", value: "" },
+                    });
+                    formik.handleChange({ target: { name: "cert", value } });
                   }}
                   label="Server Certificate"
                 />
                 <TextField
-                  label={cert ? "Certificate Password" : "Server Password"}
+                  label={
+                    formik.values.cert
+                      ? "Certificate Password"
+                      : "Server Password"
+                  }
+                  name={"password"}
                   fullWidth
                   margin="normal"
                   type="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
+                  value={formik.values.password}
+                  onChange={formik.handleChange}
+                  error={
+                    formik.touched.password && Boolean(formik.errors.password)
+                  }
+                  helperText={formik.touched.password && formik.errors.password}
                 />
               </Card>
 
@@ -119,24 +167,41 @@ function App() {
 
                 <TextField
                   label="DB Password"
+                  name={"dbPassword"}
+                  type="password"
                   fullWidth
                   margin="normal"
-                  value={dbPassword}
-                  onChange={(e) => setDbPassword(e.target.value)}
+                  value={formik.values.dbPassword}
+                  onChange={formik.handleChange}
+                  error={
+                    formik.touched.dbPassword &&
+                    Boolean(formik.errors.dbPassword)
+                  }
+                  helperText={
+                    formik.touched.dbPassword && formik.errors.dbPassword
+                  }
                 />
                 <TextField
                   label="Email"
+                  name={"email"}
                   fullWidth
                   margin="normal"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
+                  value={formik.values.email}
+                  onChange={formik.handleChange}
+                  error={formik.touched.email && Boolean(formik.errors.email)}
+                  helperText={formik.touched.email && formik.errors.email}
                 />
-                <FormControl fullWidth margin="normal">
+                <FormControl
+                  fullWidth
+                  margin="normal"
+                  error={formik.touched.timezone && formik.errors.timezone}
+                >
                   <InputLabel>Timezone</InputLabel>
                   <Select
-                    value={timezone}
-                    onChange={(e) => setTimezone(e.target.value)}
+                    value={formik.values.timezone}
+                    onChange={formik.handleChange}
                     label="Timezone"
+                    name="timezone"
                   >
                     {timezones.map((tz) => (
                       <MenuItem key={tz} value={tz}>
@@ -144,70 +209,92 @@ function App() {
                       </MenuItem>
                     ))}
                   </Select>
+                  {formik.touched.timezone && formik.errors.timezone && (
+                    <FormHelperText error>
+                      {formik.errors.timezone}
+                    </FormHelperText>
+                  )}
                 </FormControl>
                 <TextField
                   label="Pterodactyl Username"
+                  name={"pteroUsername"}
                   fullWidth
                   margin="normal"
-                  value={pteroUsername}
-                  onChange={(e) => setPteroUsername(e.target.value)}
+                  value={formik.values.pteroUsername}
+                  onChange={formik.handleChange}
+                  error={
+                    formik.touched.pteroUsername &&
+                    Boolean(formik.errors.pteroUsername)
+                  }
+                  helperText={
+                    formik.touched.pteroUsername && formik.errors.pteroUsername
+                  }
                 />
                 <TextField
                   label="First Name"
+                  name={"firstName"}
                   fullWidth
                   margin="normal"
-                  value={firstName}
-                  onChange={(e) => setFirstName(e.target.value)}
+                  value={formik.values.firstName}
+                  onChange={formik.handleChange}
+                  error={
+                    formik.touched.firstName && Boolean(formik.errors.firstName)
+                  }
+                  helperText={
+                    formik.touched.firstName && formik.errors.firstName
+                  }
                 />
                 <TextField
                   label="Last Name"
+                  name={"lastName"}
                   fullWidth
                   margin="normal"
-                  value={lastName}
-                  onChange={(e) => setLastName(e.target.value)}
+                  value={formik.values.lastName}
+                  onChange={formik.handleChange}
+                  error={
+                    formik.touched.lastName && Boolean(formik.errors.lastName)
+                  }
+                  helperText={formik.touched.lastName && formik.errors.lastName}
                 />
                 <TextField
                   label="Pterodactyl Password"
+                  name={"pteroPassword"}
                   fullWidth
                   margin="normal"
                   type="password"
-                  value={pteroPassword}
-                  onChange={(e) => setPteroPassword(e.target.value)}
+                  value={formik.values.pteroPassword}
+                  onChange={formik.handleChange}
+                  error={
+                    formik.touched.pteroPassword &&
+                    Boolean(formik.errors.pteroPassword)
+                  }
+                  helperText={
+                    formik.touched.pteroPassword && formik.errors.pteroPassword
+                  }
                 />
               </Card>
 
               <FormControlLabel
                 control={
                   <Checkbox
-                    checked={agree}
-                    onChange={(e) => setAgree(e.target.checked)}
+                    checked={formik.values.agree}
+                    name="agree"
+                    onChange={formik.handleChange}
                     color="primary"
                   />
                 }
-                label="I agree that the provided informations are saved during the installation process."
+                label="I agree that the provided information is saved during the installation process."
               />
 
               <Button
+                loadingPosition="start"
                 variant="contained"
                 color="primary"
                 fullWidth
                 style={{ marginTop: "20px" }}
-                disabled={!agree}
+                disabled={!formik.values.agree || install.isPending}
                 loading={install.isPending}
-                onClick={() =>
-                  install.mutate({
-                    serverIp,
-                    cert,
-                    password,
-                    dbPassword,
-                    email,
-                    timezone,
-                    pteroUsername,
-                    firstName,
-                    lastName,
-                    pteroPassword,
-                  })
-                }
+                onClick={formik.handleSubmit}
               >
                 Install Server
               </Button>
--- src/theme.jsx
+++ src/theme.jsx
@@ -1,112 +1,104 @@
-import { createTheme } from '@mui/material/styles';
+import { createTheme } from "@mui/material/styles";
 
 // Define your color palette
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#00C4CC',  // Bright cyan for a modern, techy feel
-      contrastText: '#FFFFFF',  // White text for better contrast
+      main: "#00C4CC", // Bright cyan for a modern, techy feel
+      contrastText: "#FFFFFF", // White text for better contrast
     },
     secondary: {
-      main: '#FFD700',  // Golden yellow for highlights and buttons
-      contrastText: '#000000',  // Dark text on yellow for visibility
+      main: "#FFD700", // Golden yellow for highlights and buttons
+      contrastText: "#000000", // Dark text on yellow for visibility
     },
     background: {
-      default: '#181818',  // Dark background to give a "gaming" feel without being overwhelming
-      paper: '#222222',  // Slightly lighter background for cards/panels
+      default: "#181818", // Dark background to give a "gaming" feel without being overwhelming
+      paper: "#222222", // Slightly lighter background for cards/panels
     },
     text: {
-      primary: '#E0E0E0',  // Light gray text for readability
-      secondary: '#B0B0B0',  // Lighter secondary text color
+      primary: "#E0E0E0", // Light gray text for readability
+      secondary: "#B0B0B0", // Lighter secondary text color
     },
     action: {
-      active: '#FFD700',  // Gold accent for interactive elements
-      hover: '#00C4CC',  // Cyan hover effect for buttons/links
+      active: "#FFD700", // Gold accent for interactive elements
+      hover: "#00C4CC", // Cyan hover effect for buttons/links
     },
   },
   typography: {
-    fontFamily: 'Roboto, Arial, sans-serif',  // Clean, modern font
+    fontFamily: "Roboto, Arial, sans-serif", // Clean, modern font
     h1: {
-      fontSize: '2.5rem',
+      fontSize: "2.5rem",
       fontWeight: 700,
-      color: '#FFD700',  // Yellow highlights for headers to make them stand out
+      color: "#FFD700", // Yellow highlights for headers to make them stand out
     },
     h2: {
-      fontSize: '2rem',
+      fontSize: "2rem",
       fontWeight: 600,
     },
     h3: {
-      fontSize: '1.5rem',
+      fontSize: "1.5rem",
       fontWeight: 500,
     },
     subtitle1: {
-      fontSize: '1rem',
-      color: '#B0B0B0',  // Slightly lighter text for secondary content
+      fontSize: "1rem",
+      color: "#B0B0B0", // Slightly lighter text for secondary content
     },
     body1: {
-      fontSize: '1rem',
-      color: '#E0E0E0',  // Easy-to-read text for paragraphs
+      fontSize: "1rem",
+      color: "#E0E0E0", // Easy-to-read text for paragraphs
     },
     button: {
-      textTransform: 'none',
+      textTransform: "none",
       fontWeight: 600,
     },
   },
   components: {
     MuiButton: {
-        styleOverrides: {
-          root: {
-            background: 'linear-gradient(45deg, #00C4CC, #FFD700)',
-            color: '#fff',
-            fontWeight: 700,
-            padding: '10px 20px',
-            borderRadius: '8px',
-            boxShadow: '0px 4px 15px rgba(0, 0, 0, 0.3)', // Default shadow
-            textShadow: '1px 1px 2px rgba(0, 0, 0, 0.8)',
-      
-            '&:hover': {
-              outline: '2px solid rgba(255, 215, 0, 1)', // Added golden outline on hover
-            },
-      
-            '&.Mui-disabled': {
-              background: 'linear-gradient(45deg, rgba(0, 196, 204, 0.2), rgba(255, 215, 0, 0.2))',
-              color: '#666',
-              boxShadow: 'none',
-              cursor: 'not-allowed',
-            },
-      
-            // Remove the white outline when focused (clicked)
-            '&:focus': {
-                outline: '2px solid rgba(255, 215, 0, 1)', // Added golden outline on hover
-              boxShadow: '0px 4px 15px rgba(0, 0, 0, 0.3)', // Keep normal box shadow after focus
-            },
+      styleOverrides: {
+        root: {
+          background: "linear-gradient(45deg, #00C4CC, #FFD700)",
+          color: "#fff",
+          fontWeight: 700,
+          padding: "10px 20px",
+          borderRadius: "8px",
+          boxShadow: "0px 4px 15px rgba(0, 0, 0, 0.3)", // Default shadow
+          textShadow: "1px 1px 2px rgba(0, 0, 0, 0.8)",
+
+          "&:hover": {
+            outline: "2px solid rgba(255, 215, 0, 1)", // Added golden outline on hover
+          },
+
+          "&.Mui-disabled": {
+            background:
+              "linear-gradient(45deg, rgba(0, 196, 204, 0.2), rgba(255, 215, 0, 0.2))",
+            color: "#666",
+            boxShadow: "none",
+            cursor: "not-allowed",
+          },
+
+          // Remove the white outline when focused (clicked)
+          "&:focus": {
+            outline: "2px solid rgba(255, 215, 0, 1)", // Added golden outline on hover
+            boxShadow: "0px 4px 15px rgba(0, 0, 0, 0.3)", // Keep normal box shadow after focus
           },
         },
       },
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
+    },
+
     MuiCard: {
       styleOverrides: {
         root: {
-          backgroundColor: '#222222',  // Dark card background to match the general theme
-          borderRadius: '8px',
-          boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.5)',
+          backgroundColor: "#222222", // Dark card background to match the general theme
+          borderRadius: "8px",
+          boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.5)",
         },
       },
     },
     MuiPaper: {
       styleOverrides: {
         root: {
-          backgroundColor: '#222222',
-          borderRadius: '8px',
+          backgroundColor: "#222222",
+          borderRadius: "8px",
         },
       },
     },
